ac1f798b812c0c8741fb22dce9fa94071119f8d0
unexpectedhaki <tpritchard843@gmail.com>
2023-10-20 15:56:05 -0400

    Removed old fetch call

--- js/main.js
+++ js/main.js
@@ -1,21 +1,6 @@
 //Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', renderDefinitions)
 
-// function getFetch(){
-//   const choice = document.querySelector('input').value
-
-//   fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${choice}`)
-//       .then(res => res.json()) // parse response as JSON
-//       .then(data => {
-//         console.log(data)
-//         document.querySelector('.word-name').innerText = data.word;
-//       })
-//       .catch(err => {
-//           console.log(`error ${err}`)
-//       });
-// }
-
-
 async function getFetch() {
   const choice = document.querySelector('input').value;
   try {
